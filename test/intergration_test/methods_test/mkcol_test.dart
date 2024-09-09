c049d108fff6cdf36f7525d1aa25e9ff6ac90e30
Fries_I23 <FriesI23@outlook.com>
2024-09-09 17:55:55 +0800

    add rfc4918 9.3.2 example test case

---
+++ test/intergration_test/methods_test/mkcol_test.dart
@@ -0,0 +1,74 @@
+// Copyright (c) 2024 Fries_I23
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:simple_webdav_client/client.dart';
+import 'package:test/test.dart';
+
+import '../server.dart';
+
+void main() {
+  group("test mkcol", () {
+    late final TestUsagedHttpServer server;
+    late final WebDavStdClient client;
+    late final Uri addr;
+
+    setUpAll(() async {
+      server = TestUsagedHttpServer();
+      await server.open();
+      client = WebDavClient.std();
+      addr = Uri(scheme: 'http', host: "localhost", port: server.bindPort);
+    });
+
+    tearDownAll(() async {
+      client.close();
+      server.close();
+    });
+
+    setUp(() {
+      server.expectedResponse = null;
+      server.serverSideChecker = null;
+    });
+
+    test(
+        "RFC4918 9.3.2 MKCOL, "
+        "see: https://datatracker.ietf.org/doc/html/rfc4918#section-9.3.2",
+        () async {
+      final requestBody = '';
+      final responseBody = '';
+
+      Future<bool> expectedResponse(HttpRequest event) async {
+        event.response.statusCode = HttpStatus.created;
+        event.response.contentLength = responseBody.length;
+        event.response.write(responseBody);
+        return true;
+      }
+
+      server.expectedResponse = expectedResponse;
+
+      Future<void> serverSideChecker(HttpRequest event) async {
+        expect(event.method, WebDavMethod.mkcol.name);
+        expect(event.headers.contentType, isNull);
+        final body = await utf8.decodeStream(event);
+        expect(body, requestBody);
+      }
+
+      server.serverSideChecker = serverSideChecker;
+
+      final request = await client.dispatch(addr).createDir();
+      final response = await request.close();
+      final result = await response.parse();
+      expect(response.body, responseBody);
+      expect(result!.length, 1);
+      expect(result.first.path, equals(addr));
+      expect(result.first.props, isEmpty);
+      expect(result.first.status, HttpStatus.created);
+      expect(result.first.error, isNull);
+      expect(result.first.desc, isNull);
+    });
+  });
+}
--- test/intergration_test/methods_test/proppatch_test.dart
+++ test/intergration_test/methods_test/proppatch_test.dart
@@ -31,34 +31,35 @@ final class Authors implements ToXmlCapable {
 }
 
 void main() {
-  late final TestUsagedHttpServer server;
-  late final WebDavStdClient client;
-  late final Uri addr;
+  group("test proppatch", () {
+    late final TestUsagedHttpServer server;
+    late final WebDavStdClient client;
+    late final Uri addr;
 
-  setUpAll(() async {
-    server = TestUsagedHttpServer();
-    await server.open();
-    client = WebDavClient.std();
-    addr = Uri(scheme: 'http', host: "localhost", port: server.bindPort);
-  });
+    setUpAll(() async {
+      server = TestUsagedHttpServer();
+      await server.open();
+      client = WebDavClient.std();
+      addr = Uri(scheme: 'http', host: "localhost", port: server.bindPort);
+    });
 
-  tearDownAll(() async {
-    client.close();
-    server.close();
-  });
+    tearDownAll(() async {
+      client.close();
+      server.close();
+    });
 
-  setUp(() {
-    server.expectedResponse = null;
-    server.serverSideChecker = null;
-  });
+    setUp(() {
+      server.expectedResponse = null;
+      server.serverSideChecker = null;
+    });
 
-  test(
-      "RFC4918 9.2.2 PROPPATCH, "
-      "see: https://datatracker.ietf.org/doc/html/rfc4918#section-9.2.2",
-      () async {
-    final ns = "http://ns.example.com/standards/z39.50/";
+    test(
+        "RFC4918 9.2.2 PROPPATCH, "
+        "see: https://datatracker.ietf.org/doc/html/rfc4918#section-9.2.2",
+        () async {
+      final ns = "http://ns.example.com/standards/z39.50/";
 
-    final requestBody = '''
+      final requestBody = '''
 <?xml version="1.0" encoding="utf-8"?>
 <a1:propertyupdate xmlns:a1="DAV:" xmlns:a2="http://ns.example.com/standards/z39.50/">
   <a1:set>
@@ -76,9 +77,9 @@ void main() {
   </a1:remove>
 </a1:propertyupdate>
 '''
-        .trim();
+          .trim();
 
-    final responseBody = '''
+      final responseBody = '''
 <?xml version="1.0" encoding="utf-8" ?>
 <D:multistatus xmlns:D="DAV:"
         xmlns:Z="http://ns.example.com/standards/z39.50/">
@@ -98,84 +99,86 @@ void main() {
   </D:response>
 </D:multistatus>
 '''
-        .trim();
+          .trim();
 
-    Future<bool> expectedResponse(HttpRequest event) async {
-      event.response.statusCode = HttpStatus.multiStatus;
-      event.response.headers.contentType =
-          ContentType.parse('application/xml; charset="utf-8"');
-      event.response.contentLength = responseBody.length;
-      event.response.write(responseBody);
-      return true;
-    }
+      Future<bool> expectedResponse(HttpRequest event) async {
+        event.response.statusCode = HttpStatus.multiStatus;
+        event.response.headers.contentType =
+            ContentType.parse('application/xml; charset="utf-8"');
+        event.response.contentLength = responseBody.length;
+        event.response.write(responseBody);
+        return true;
+      }
 
-    server.expectedResponse = expectedResponse;
+      server.expectedResponse = expectedResponse;
 
-    final propParsers = Map.of(kStdPropParserManager);
-    propParsers[(name: "Authors", ns: ns)] =
-        const TestUsageXmlStringPropParser();
-    propParsers[(name: "Copyright-Owner", ns: ns)] =
-        const TestUsageXmlStringPropParser();
+      final propParsers = Map.of(kStdPropParserManager);
+      propParsers[(name: "Authors", ns: ns)] =
+          const TestUsageXmlStringPropParser();
+      propParsers[(name: "Copyright-Owner", ns: ns)] =
+          const TestUsageXmlStringPropParser();
 
-    final propstatParser = BasePropstatElementParser(
-        parserManger: WebDavResposneDataParserManger(parsers: propParsers),
-        statusParser: const BaseHttpStatusElementParser(),
-        errorParser: const BaseErrorElementParser());
-    final responseParser = BaseResponseElementParser(
-        hrefParser: const BaseHrefElementParser(),
-        statusParser: const BaseHttpStatusElementParser(),
-        propstatParser: propstatParser,
-        errorParser: const BaseErrorElementParser(),
-        locationParser: const BaseHrefElementParser());
-    final multistatParser =
-        BaseMultistatusElementParser(responseParser: responseParser);
+      final propstatParser = BasePropstatElementParser(
+          parserManger: WebDavResposneDataParserManger(parsers: propParsers),
+          statusParser: const BaseHttpStatusElementParser(),
+          errorParser: const BaseErrorElementParser());
+      final responseParser = BaseResponseElementParser(
+          hrefParser: const BaseHrefElementParser(),
+          statusParser: const BaseHttpStatusElementParser(),
+          propstatParser: propstatParser,
+          errorParser: const BaseErrorElementParser(),
+          locationParser: const BaseHrefElementParser());
+      final multistatParser =
+          BaseMultistatusElementParser(responseParser: responseParser);
 
-    final parsers = Map.of(kStdElementParserManager);
-    parsers[(name: WebDavElementNames.multistatus, ns: kDavNamespaceUrlStr)] =
-        multistatParser;
+      final parsers = Map.of(kStdElementParserManager);
+      parsers[(name: WebDavElementNames.multistatus, ns: kDavNamespaceUrlStr)] =
+          multistatParser;
 
-    final resultParser = BaseRespMultiResultParser(
-        parserManger: WebDavResposneDataParserManger(parsers: parsers));
+      final resultParser = BaseRespMultiResultParser(
+          parserManger: WebDavResposneDataParserManger(parsers: parsers));
 
-    final request = await client
-        .dispatch(addr, responseResultParser: resultParser)
-        .updateProps(operations: [
-      ProppatchRequestProp.set(
-          name: "Authors",
-          namespace: ns,
-          value: Authors(["Jim Whitehead", "Roy Fielding"], ns)),
-      ProppatchRequestProp.remove(name: "Copyright-Owner", namespace: ns),
-    ]);
+      final request = await client
+          .dispatch(addr, responseResultParser: resultParser)
+          .updateProps(operations: [
+        ProppatchRequestProp.set(
+            name: "Authors",
+            namespace: ns,
+            value: Authors(["Jim Whitehead", "Roy Fielding"], ns)),
+        ProppatchRequestProp.remove(name: "Copyright-Owner", namespace: ns),
+      ]);
 
-    Future<void> serverSideChecker(HttpRequest event) async {
-      expect(event.headers["Depth"], isNull);
-      expect(event.headers.contentType.toString(),
-          equals(XmlContentType.applicationXml.toString()));
-      final body = await utf8.decodeStream(event);
-      expect(XmlDocument.parse(body).toXmlString(pretty: true), requestBody);
-    }
+      Future<void> serverSideChecker(HttpRequest event) async {
+        expect(event.headers["Depth"], isNull);
+        expect(event.headers.contentType.toString(),
+            equals(XmlContentType.applicationXml.toString()));
+        final body = await utf8.decodeStream(event);
+        expect(XmlDocument.parse(body).toXmlString(pretty: true), requestBody);
+      }
 
-    server.serverSideChecker = serverSideChecker;
+      server.serverSideChecker = serverSideChecker;
 
-    final response = await request.close();
-    expect(response.body, isNull);
-    final result = await response.parse();
-    expect(response.body, equals(responseBody));
-    expect(result!.length, 1);
-    expect(result.first.status, HttpStatus.multiStatus);
-    expect(result.first.path, Uri.parse("http://www.example.com/bar.html"));
-    expect(result.first.error, isNull);
-    expect(result.first.props.length, 2);
-    expect(result.first.desc, 'Copyright Owner cannot be deleted or altered.');
-    final prop1 = result.first.find("Authors", emptyNamespace: false).first;
-    expect(prop1.name, "Authors");
-    expect(prop1.namespace, equals(Uri.parse(ns)));
-    expect(prop1.status, HttpStatus.failedDependency);
-    expect(prop1.value, '');
-    final prop2 = result.first.find("Copyright-Owner", namespace: ns).first;
-    expect(prop2.name, "Copyright-Owner");
-    expect(prop2.namespace, equals(Uri.parse(ns)));
-    expect(prop2.status, HttpStatus.conflict);
-    expect(prop2.value, '');
+      final response = await request.close();
+      expect(response.body, isNull);
+      final result = await response.parse();
+      expect(response.body, equals(responseBody));
+      expect(result!.length, 1);
+      expect(result.first.status, HttpStatus.multiStatus);
+      expect(result.first.path, Uri.parse("http://www.example.com/bar.html"));
+      expect(result.first.error, isNull);
+      expect(result.first.props.length, 2);
+      expect(
+          result.first.desc, 'Copyright Owner cannot be deleted or altered.');
+      final prop1 = result.first.find("Authors", emptyNamespace: false).first;
+      expect(prop1.name, "Authors");
+      expect(prop1.namespace, equals(Uri.parse(ns)));
+      expect(prop1.status, HttpStatus.failedDependency);
+      expect(prop1.value, '');
+      final prop2 = result.first.find("Copyright-Owner", namespace: ns).first;
+      expect(prop2.name, "Copyright-Owner");
+      expect(prop2.namespace, equals(Uri.parse(ns)));
+      expect(prop2.status, HttpStatus.conflict);
+      expect(prop2.value, '');
+    });
   });
 }
